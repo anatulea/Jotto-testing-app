47989117cf60d670190f3f7961cacf8ac198564a
Ana Tulea <ana@Anas-MacBook-Pro.local>
2020-06-30 16:23:40 -0700

    Added secret word and tested to make sure is a string

--- src/Input.test.js
+++ src/Input.test.js
@@ -2,13 +2,16 @@ import React from "react";
 import Input from "./Input";
 import { shallow } from "enzyme";
 
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
-const setup = (props = {}) => {
-  return shallow(<Input />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 test("Input renders without error", () => {
   const wrapper = setup();
   const inputComponent = findByTestAttr(wrapper, "component-input");
   expect(inputComponent.length).toBe(1);
+});
+test("does not throw an error with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
 });
