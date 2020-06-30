cc5912f441f281a5c2534ea86c1190e26c5e0cba
Ana Tulea <ana@Anas-MacBook-Pro.local>
2020-06-29 21:07:54 -0700

    added tabele in guessed words component

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,20 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function GuessedWords(props) {
-    let contents;
-    if(props.guessedWords.length === 0){
-        contents =(
-            <span data-test ="guesse-instructions">
-                Try to guess the secret word!
-            </span>
-        )
-    }
-  return <div data-test="component-guessed-words">
-      {contents}
-  </div>;
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guesse-instructions">Try to guess the secret word!</span>
+    );
+  } else {
+    const guessedWordsRow = props.guessedWords.map((word, index) => (
+      <tr data-test="guessed-word" key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRow}</tbody>
+        </table>
+      </div>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
 }
-
-
 
 GuessedWords.propTypes = {
   guessedWords: PropTypes.arrayOf(
