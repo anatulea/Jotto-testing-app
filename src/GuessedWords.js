618ad9142f3fb99e54e34b7b21f227c05948e904
Ana Tulea <ana@Anas-MacBook-Pro.local>
2020-06-29 20:44:24 -0700

    start tests for guesswords

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,14 +1,29 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function GuessedWords(props) {
-  
-    return (
-      <div data-test="">
-        
-      </div>
-    );
-  
+    let contents;
+    if(props.guessedWords.length === 0){
+        contents =(
+            <span data-test ="guesse-instructions">
+                Try to guess the secret word!
+            </span>
+        )
+    }
+  return <div data-test="component-guessed-words">
+      {contents}
+  </div>;
 }
+
+
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default GuessedWords;
